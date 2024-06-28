dc8ba5b40e19fa493d807727753d612e6939d65d
saanyay <saanyajauhri@gmail.com>
2024-06-28 00:34:35 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -38,18 +38,15 @@ public class Main {
                     int timesteps = 0;
                     List<Cell> path = (bot_1.breadthFirstSearch(ship, hProbCell));
 
-                    // Test Statements
-                    // System.out.println("Prob of bot location: " + ship.calculateProb(botLocation));
-                    // System.out.println("The cell w highest Prob: " + hProbCell.toString());
-                    // System.out.println("Mouse Location: " + mouseLocation.toString());
-                    
                     for(int i = 0; i < path.size(); i++){
-                        System.out.println("================");
+                        System.out.println("=================================");
                         System.out.println("Bot Location: " + botLocation.toString());
                         ship.setBot(botLocation.getX(), botLocation.getY());
                         if(i != 0){
+                            // Comment off lines 47 & 48 for a stationary mouse
                             Cell tempMouse = mouse.moveMouse(mouseLocation);
                             mouseLocation.setLocation(tempMouse.getX(), tempMouse.getY());
+                            System.out.println("Mouse Location: " + mouseLocation.toString());
                         }
                         if(i == 0){
                             for(Cell cell : path){
@@ -74,11 +71,113 @@ public class Main {
                             needRecalculation = true;
                         }
                         if(botLocation.getX() == ship.getMouse().getX() && botLocation.getY() == ship.getMouse().getY()){
+                            ship.printCompleteGrid();
                             System.out.println("Bot reached the button (2).");
                             System.out.println("Bot reached the button in " + timesteps + " timesteps.");
                             needRecalculation = false;
                             break;
                         }
+                    }
+                }
+                break;
+            }
+            if(menuOption.equals("2")){ // Bot 2
+                // Set Up & Initialization
+                Ship ship = new Ship(D);
+                int alternateMoves = 1; 
+
+                // Bot
+                Bot_2 bot_2 = new Bot_2(ship);
+                Cell botLocation = bot_2.placeBot();
+                ship.setBot(botLocation.getX(), botLocation.getY());
+
+                // Mouse
+                Mouse mouse = new Mouse (ship);
+                Cell mouseLocation = mouse.placeMouse();
+                ship.setMouse(mouseLocation.getX(), mouseLocation.getY());
+                mouseLocation.setLocation(mouseLocation.getX(), mouseLocation.getY());
+
+                int timesteps = 0;
+
+                while(true){
+                    System.out.println("===============================");
+                    System.out.println("Bot Location: " + botLocation.toString());
+                    ship.setBot(botLocation.getX(), botLocation.getY());
+
+                    // Alternate between bot moving & sensing 
+
+                    if(alternateMoves % 2 == 0){ // Bot Moves
+
+                        System.out.println("BOT SENSED, NOW MOVE TURN");
+
+                        // Moving Mouse
+                        Cell tempMouse = mouse.moveMouse(mouseLocation);
+                        mouseLocation.setLocation(tempMouse.getX(), tempMouse.getY());
+                        System.out.println("Mouse Location: " + mouseLocation.toString());
+
+                        ship.printCompleteGrid();
+                        if(mouseLocation.getX() == botLocation.getX() && mouseLocation.getY() == botLocation.getY()){
+                            System.out.println("Bot has found the mouse in " + timesteps + " timesteps.");
+                            break;
+                        }
+                        Cell hProbCell = ship.findHProbCell();
+                        List<Cell> path = (bot_2.breadthFirstSearch(ship, hProbCell));
+                        if(path.size() > 0){
+                            System.out.println("Path size: " + path.size());
+                            if(path.size() > timesteps){
+                                timesteps = path.size();
+                            }
+                            for(Cell cell : path){
+                                System.out.print(cell.toString() + " -> ");
+                            }
+                            System.out.println();
+                            System.out.println("==============================");
+                            botLocation.setLocation(path.get(0).getX(), path.get(0).getY());
+                        }
+                        else if (path.size() == 0){
+                            System.out.println("There is no path from the bot to the button. ");
+                            break;
+                        }
+                        alternateMoves++;
+                    }
+                    else if(alternateMoves % 2 == 1){ // Bot senses
+
+                        System.out.println("BOT MOVED, NOW SENSE TURN");
+                        
+                        // Moving Mouse
+                        Cell tempMouse = mouse.moveMouse(mouseLocation);
+                        mouseLocation.setLocation(tempMouse.getX(), tempMouse.getY());
+                        System.out.println("Mouse Location: " + mouseLocation.toString());
+
+                        ship.printCompleteGrid();
+                        if(mouseLocation.getX() == botLocation.getX() && mouseLocation.getY() == botLocation.getY()){
+                            System.out.println("Bot has found the mouse in " + timesteps + " timesteps.");
+                            break;
+                        }
+                        Cell hProbCell = ship.findHProbCell();
+                        List<Cell> path = (bot_2.breadthFirstSearch(ship, hProbCell));
+                        if(path.size() > 0){
+                            System.out.println("Path size: " + path.size());
+                            if(path.size() > timesteps){
+                                timesteps = path.size();
+                            }
+                            for(Cell cell : path){
+                                System.out.print(cell.toString() + " -> ");
+                            }
+                            System.out.println();
+                            System.out.println("==============================");
+                        }
+                        else if (path.size() == 0){
+                            System.out.println("There is no path from the bot to the button. ");
+                            break;
+                        }
+                        if(botLocation.getX() == ship.getMouse().getX() && botLocation.getY() == ship.getMouse().getY()){
+                            ship.printCompleteGrid();
+                            System.out.println("Bot reached the button (2).");
+                            System.out.println("Bot reached the button in " + timesteps + " timesteps.");
+                            break;
+                        }
+                        alternateMoves++;
                     }
                 }
                 break;
